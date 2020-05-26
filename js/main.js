545a2108842acae0b6315df0f0ccfb0fd2a02b26
Dulce Flores <dulce.1201@gmail.com>
2020-05-26 23:58:37 +0200

    change background img and finished js code

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ let userScore = 0;
 let cpuScore = 0;
 const userScore_span = document.getElementById("user-score");
 const cpuScore_span = document.getElementById("cpu-score");
-const scoreBoard_div = document.getElementById(".score-board")
+// const scoreBoard_div = document.getElementById(".score-board")
 const restart = document.getElementById("restart");
 const result = document.getElementById ("result")
-const modal = document.querySelector("modal");
+const modal = document.querySelector(".modal");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+
 
 function getCpuChoice() {
   const choices = ['rock', 'paper', 'scissors'];
@@ -16,30 +17,54 @@ function getCpuChoice() {
   return choices[randomNumber];
 }
 
+
+function win(userChoice, cpuChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  result.innerHTML = `<h1 class="text-win">You win!</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`;
+  modal.style.display = 'block';
+}
+
+function lose(userChoice, cpuChoice){
+  cpuScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  result.innerHTML = `<h1 class="text-lose">You lost</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`; 
+  modal.style.display = 'block'
+}
+
+function draw(userChoice, cpuChoice){
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  result.innerHTML = `<h1>It's a draw</h1> <p>You both choose <strong>${cpuChoice}</strong></p>`;
+  modal.style.display = 'block'
+}
+
+
 function play(userChoice) {
   const cpuChoice = getCpuChoice();
   switch (userChoice + cpuChoice) {
-    case 'rock scissors':
-    case 'paper rock':
-    case 'scissors paper':
-      console.log("user wins");
-      console.log(userChoice, cpuChoice);
+    case 'rockscissors':
+    case 'paperrock':
+    case 'scissorspaper':
+      win(userChoice, cpuChoice);
       break;
-    case 'rock paper':
-    case 'paper scissors':
-    case 'scissors rock':
-      console.log("user loses");
-      console.log(userChoice, cpuChoice);
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      lose(userChoice, cpuChoice);
       break;
-    default: 
-      console.log("it's a draw");
+    case 'rockrock':
+    case 'paperpaper':
+    case 'scissorsscissors':
+      draw(userChoice, cpuChoice);
+      break;
   }
 }
 
 
 function main() {
-
-  console.log("main");
   rock_div.addEventListener('click', function() {
     play('rock');
   })
@@ -54,8 +79,21 @@ function main() {
 }
 
 
-rock_div.addEventListener('click', function() {
-  play('rock');
-})
+function clearModal(e){
+  if(e.target == modal) {
+    modal.style.display = "none"
+  }
+}
 
-// play();
+
+function restartGame(){
+  userScore = 0;
+  cpuScore = 0;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+}
+
+
+restart.addEventListener('click', restartGame);
+window.addEventListener('click', clearModal);
+main ();
