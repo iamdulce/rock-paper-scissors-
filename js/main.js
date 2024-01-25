54972628c9b5cc58866aa766f478ce0d27864cd9
Christian <146226859+KwaziiMedia@users.noreply.github.com>
2024-01-24 23:05:48 -0500

    Update main.js
    added comments for understandability you wrote amazing code!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+//Tracks UserScore and Robots Score
 let userScore = 0;
 let cpuScore = 0;
 let closeBtn;
+//Creates variables for every element in the html
 const userScore_span = document.getElementById("user-score");
 const cpuScore_span = document.getElementById("cpu-score");
 // const scoreBoard_div = document.getElementById(".score-board")
@@ -11,23 +13,29 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-
+//Function for Cpu's Choice
 function getCpuChoice() {
+  //Array full of 3 choices
   const choices = ['rock', 'paper', 'scissors'];
+  //sets random number and round it down to nearest number 
   const randomNumber = Math.floor(Math.random() * 3);
+  //returns random nunber for Cpu choice
   return choices[randomNumber];
 }
 
 
-
+//Function that checks whether user or cpu wins
 function win(userChoice, cpuChoice) {
+  //increments user score
   userScore++;
+  //changes userscore element text to userScore variable and cpu element to cpuScore
   userScore_span.innerHTML = userScore;
   cpuScore_span.innerHTML = cpuScore;
+  //shows the winner in the result span
   result.innerHTML = `<span class="close"></span> <h1 class="text-win">You win!</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`;
   modal.style.display = 'flex';
 }
-
+//function to check who loses
 function lose(userChoice, cpuChoice){
   cpuScore++;
   userScore_span.innerHTML = userScore;
@@ -35,7 +43,7 @@ function lose(userChoice, cpuChoice){
   result.innerHTML = `<span class="close"></span> <h1 class="text-lose">You lost</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`; 
   modal.style.display = 'flex'
 }
-
+//checks if there is a draw
 function draw(userChoice, cpuChoice){
   userScore_span.innerHTML = userScore;
   cpuScore_span.innerHTML = cpuScore;
@@ -43,7 +51,7 @@ function draw(userChoice, cpuChoice){
   modal.style.display = 'flex'
 }
 
-
+//starts with user choice and randomly chooses who wins the round
 function play(userChoice) {
   const cpuChoice = getCpuChoice();
   switch (userChoice + cpuChoice) {
@@ -94,7 +102,7 @@ function clearModal(e){
   }
 }
 
-
+//restarts game and sets everything to 0 and sets the elememnts to user and cpu score
 function restartGame(){
   userScore = 0;
   cpuScore = 0;
